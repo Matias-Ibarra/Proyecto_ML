48132d1d749b78978a1c56a92472fa37044013f7
Matias-Ibarra <89396537+Matias-Ibarra@users.noreply.github.com>
2023-11-29 23:23:26 +0100

    NOV29

--- app/app.py
+++ app/app.py
@@ -9,10 +9,10 @@ st.set_page_config(page_title="Adaptabilidad",
 
 color_de_fondo = "#363636"
 
-seleccion = st.sidebar.selectbox("Selecciona menu", ['Home','Datos', 'Oversampling', "Modelos de clasificación", 'Predicción'])
+seleccion = st.sidebar.selectbox("Pasos del proyecto", ['Introducción', 'Datos', 'Mapeo y Dummies', 'Oversampling', "Modelos de clasificación", 'GridSearchCV', 'Predicción', 'Feature importances'])
 
 
-if seleccion == "Home":
+if seleccion == "Introducción":
     st.markdown("<h1 style='text-align: center; background-color: #363636; color: white;'>Adaptabilidad a la educación online</h1>", unsafe_allow_html=True)
     img = Image.open("../images/alumno.jpg")
     imagen = img.resize((800, 400))
@@ -71,6 +71,55 @@ elif seleccion == "Datos":
         st.markdown("<h1 style='text-align: center; background-color: #363636; color: white;'>Datos para train</h1>", unsafe_allow_html=True)
         df = pd.read_csv("../data/train/train.csv")
         st.write(df)
+
+elif seleccion == "Mapeo y Dummies":
+    st.write("## Mapeo y Dummies")
+
+    texto = """
+            Mapeo de datos:
+
+            Variable Age = {'1-5': 0, '6-10': 1, '11-15': 2, '16-20': 3, '21-25': 4, '26-30': 5}
+
+            Variable Education Level = {'School': 0, 'College': 1, 'University': 2}
+
+            Variable IT Student = {'No': 0, 'Yes': 1}
+
+            Variable Location = {'No': 0, 'Yes': 1}
+
+            Variable Load-shedding = {'Low': 0, 'High': 1}
+
+            Variable Financial Condition = {'Poor': 0, 'Mid': 1, 'Rich': 2}
+
+            Variable Internet Type = {'Mobile Data': 0, 'Wifi': 1}
+
+            Variable Network Type = {'2G': 0, '3G': 1, '4G': 2}
+
+            Variable Class Duration = {'0': 0, '1-3': 1, '3-6': 2}
+
+            Variable Self Lms = {'No': 0, 'Yes': 1}
+
+            Variable Device = {'Mobile': 0, 'Tab': 1, 'Computer': 2}
+
+            Adaptivity Level = {'Low': 0, 'Moderate': 1, 'High': 2}
+            
+            
+            get_dummies():
+
+            Cada variable se convierte en tantas variables 0/1 como valores diferentes haya. Cada una de las columnas de la salida lleva el nombre de un valor;
+            si la entrada es un DataFrame, el nombre de la variable original se antepone al valor.
+            [Fuente](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.get_dummies.html)
+
+            Aplicado a las variables Institution Type y Gender:
+
+            Antes:
+            """
+    st.write(texto)
+    img = Image.open("../images/antes_dummies.png")
+    st.image(img)
+
+    st.write('Después:')
+    img = Image.open("../images/despues_dummies.png")
+    st.image(img)
 
 elif seleccion == "Oversampling":
     st.write("## Oversampling")
@@ -182,9 +231,11 @@ elif seleccion == "Modelos de clasificación":
 
     st.write("#### PCA + SVC:")
     texto = """
-            Un clasificador AdaBoost es un metaestimador que comienza ajustando un clasificador en el conjunto de datos original y luego ajusta copias adicionales del clasificador en el mismo conjunto de datos, pero donde los pesos de las instancias clasificadas incorrectamente se ajustan de modo que los clasificadores posteriores se centren más en casos difíciles.
+            El análisis de componentes principales (PCA) es un algoritmo para transformar las columnas de un conjunto de datos en un nuevo conjunto de características llamadas Componentes principales.
+            Al hacer esto, una gran parte de la información en todo el conjunto de datos se comprime de manera efectiva en menos columnas de características.
+            Esto permite la reducción de la dimensionalidad y la capacidad de visualizar la separación de clases o grupos, si los hubiera.
 
-            [Fuente](https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostClassifier.html)
+            [Fuente](https://mlstudio.jaol.net/principal-components-analysis-pca-better-explained/)
     
             Resultados:
     """
@@ -194,6 +245,27 @@ elif seleccion == "Modelos de clasificación":
 
     texto = "Accuracy: 87,7 %"
     st.write(texto)    
+
+elif seleccion == "GridSearchCV":
+    st.write("## GridSearchCV")
+    texto = """
+            Búsqueda exhaustiva de valores de parámetros específicos para un estimador.
+            Los parámetros del estimador utilizado para aplicar estos métodos se optimizan mediante una búsqueda de cuadrícula con validación cruzada sobre una cuadrícula de parámetros.
+            [Fuente](https://scikit-learn.org/0.17/modules/generated/sklearn.grid_search.GridSearchCV.html)
+
+            CrossValidation:
+            El conjunto de entrenamiento se divide en k conjuntos más pequeños.
+            Se sigue el siguiente procedimiento para cada uno de los k “pliegues”
+            
+            La medida de rendimiento informada por la validación cruzada de k veces es entonces el promedio de los valores calculados en el bucle.
+            Este enfoque puede ser costoso desde el punto de vista computacional, pero no desperdicia demasiados datos (como es el caso cuando se fija un conjunto de validación arbitrario),
+            lo cual es una gran ventaja en problemas como la inferencia inversa donde el número de muestras es muy pequeño.
+            [Fuente](https://scikit-learn.org/stable/modules/cross_validation.html)
+            """
+    st.write(texto)
+
+    img = Image.open("../images/CrossValidation.jpg")
+    st.image(img)
 
 elif seleccion == "Predicción":
     st.write("## Página de Entrenamiento")
@@ -214,8 +286,8 @@ elif seleccion == "Predicción":
     st.text('"Bajo": 0 - "Moderado": 1 - "Alto": 2')
     respuesta_pregunta8 = st.selectbox("Condición económica del estudiante", [0, 1, 2])
     st.text('"Baja": 0 - "Media": 1 - "Alta": 2')
-    respuesta_pregunta9 = st.selectbox("Tipo de conexión a internet", [0, 1, 2])
-    st.text('"Conexión por móvil": 0 - "WiFi": 1 - "Otro": 2')
+    respuesta_pregunta9 = st.selectbox("Tipo de conexión a internet", [0, 1])
+    st.text('"Conexión por móvil": 0 - "WiFi": 1')
     respuesta_pregunta10 = st.selectbox("Tipo de red", [0, 1, 2])
     st.text('"2G": 0 - "3G": 1 - "4G": 2')
     respuesta_pregunta11 = st.selectbox("Duración (en horas) de las clases", [0, 1, 2])
@@ -246,3 +318,17 @@ elif seleccion == "Predicción":
    
     if st.button('Realizar predicción'):
         st.success(modelo_svc.predict(features)[0])
+        if modelo_svc.predict(features)[0] == 0:
+            st.text('Baja adaptabilidad')
+        elif modelo_svc.predict(features)[0] == 1:
+            st.text('Adaptabilidad Moderada')
+        else:
+            st.text('Alta adaptabilidad')
+
+elif seleccion == 'Feature importances':
+    st.write("## Feature importances")
+
+    img = Image.open("../images/feature_importances.png")
+    imagen = img.resize((400, 650))
+    st.image(imagen)
+
